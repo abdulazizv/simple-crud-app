fcb977874031d1da8f04e4d2bc8e4dbd7a5b74e9
Abdulazizv <abdulazizheir@gmail.com>
2023-02-09 10:25:14 +0500

    logout added in script.js

--- src/script.js
+++ src/script.js
@@ -1,2 +1,17 @@
 "use strict";
 
+let exit = document.querySelector('#exit');
+
+
+//logout
+
+exit.addEventListener('click',() => {
+  localStorage.clear()
+});
+
+(function logout() {
+  let token = localStorage.getItem('token');
+  if (!token) {
+    window.location.replace('./login.html');
+  }
+})()
